23b51a7f19c56ca0769542ee85813f254d684adf
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-07-02 19:51:29 -0300

    Corrige build

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,6 @@ import os
 
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,4 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+django_heroku.settings(locals())
